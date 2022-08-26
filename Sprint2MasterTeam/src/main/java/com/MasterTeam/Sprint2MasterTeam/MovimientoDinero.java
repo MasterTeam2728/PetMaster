b331d779c16936388fe3b5767c9fc485b8962f69
CristianEspinosa90 <kicham90@gmail.com>
2022-08-25 22:13:52 -0500

    Se modelo el movimiento de dinero como una clase java

--- Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/MovimientoDinero.java
+++ Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/MovimientoDinero.java
@@ -1,6 +1,47 @@
 package com.MasterTeam.Sprint2MasterTeam;
 
 public class MovimientoDinero {
+    private double montoMovimiento;
+    private double monto;
+    private String conceptoMovimiento;
+    private String usuarioMovimiento;
 
+    public MovimientoDinero(double montoMovimiento, double monto, String conceptoMovimiento, String usuarioMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+        this.monto = monto;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.usuarioMovimiento = usuarioMovimiento;
+    }
 
+    public double getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(double montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getUsuarioMovimiento() {
+        return usuarioMovimiento;
+    }
+
+    public void setUsuarioMovimiento(String usuarioMovimiento) {
+        this.usuarioMovimiento = usuarioMovimiento;
+    }
 }
