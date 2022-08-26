374b66bccfe2b4be763902e71fc7b3ffe9a87273
davgom20 <daesgomez20@gmail.com>
2022-08-25 20:58:20 -0500

    clase empresa creada

--- Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/Empresa.java
+++ Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/Empresa.java
@@ -2,5 +2,47 @@ package com.MasterTeam.Sprint2MasterTeam;
 
 public class Empresa {
 
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private Integer telefonoEmpresa;
+    private Integer nitEmpresa;
 
+    public Empresa(String nombreEmpresa, String direccionEmpresa, Integer telefonoEmpresa, Integer nitEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.nitEmpresa = nitEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public Integer getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(Integer telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public Integer getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(Integer nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
 }
