d2f5e3b53a8e9dd55f8763f72eea3d65165de408
Nicolas Ortiz <vinico0911@hotmail.com>
2022-08-26 22:05:28 -0500

    Creacion de la clase Empleado

--- Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/Empleado.java
+++ Sprint2MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/Empleado.java
@@ -1,6 +1,53 @@
 package com.MasterTeam.Sprint2MasterTeam;
 
 public class Empleado {
+    //Nicolas Ortiz coding
+
+    private String nombre;
+    private String correo;
+    private String empresaEmpleadoPertenece;
+    private String rolEmpleado;
+
+    //Constructor method
+    public Empleado(String nombre, String correo, String empresaEmpleadoPertenece, String rolEmpleado) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresaEmpleadoPertenece = empresaEmpleadoPertenece;
+        this.rolEmpleado = rolEmpleado;
+    }
+
+    //Getters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEmpresaEmpleadoPertenece() {
+        return empresaEmpleadoPertenece;
+    }
+
+    public void setEmpresaEmpleadoPertenece(String empresaEmpleadoPertenece) {
+        this.empresaEmpleadoPertenece = empresaEmpleadoPertenece;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
 
 
 }
