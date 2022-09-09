a9ec2ca6ae1a657b4b37b5e2ae09399c5e1fcf64
CristianEspinosa90 <kicham90@gmail.com>
2022-09-09 17:43:33 -0500

    se crean los controladores y servicios de MOVIMIENTODINERO, con la ruta de /enterprises/[id]/movements

--- Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/controladores/MovimientoEmpresaControlador.java
+++ Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/controladores/MovimientoEmpresaControlador.java
@@ -1,7 +1,11 @@
 package com.MasterTeam.Sprint2MasterTeam.controladores;
 
+import com.MasterTeam.Sprint2MasterTeam.entidades.Empresa;
+import com.MasterTeam.Sprint2MasterTeam.entidades.MovimientoDinero;
 import com.MasterTeam.Sprint2MasterTeam.servicios.MovimientoDineroServicios;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class MovimientoEmpresaControlador {
@@ -13,4 +17,22 @@ public class MovimientoEmpresaControlador {
     }
     //El sistema devuelve reponses 200 en la ruta /enterprises/[id]/movements con los siguientes
     //verbos: GET, POST, PATCH, DELETE
+
+    @GetMapping("/enterprises/[id]/movements")
+    public List<MovimientoDinero> listaMovimientoDinero() {
+        return serviciosMov.getRepositorioMov();
+    }
+
+    //Metodo crear un movimiento de dinero
+    @PostMapping("/enterprises/[id]/movements")
+    public MovimientoDinero crearMovimientoDinero(@RequestBody MovimientoDinero Mov) {
+        return this.serviciosMov.crearMovimientoDinero(Mov);
+    }
+
+    @DeleteMapping("/enterprises/[id]/movements")
+    public MovimientoDinero eliminarMovimientoDinero(MovimientoDinero DeleteMov){
+        return this.serviciosMov.eliminarMovimientoDinero(DeleteMov);
+    }
+
+
 }
--- Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/servicios/MovimientoDineroServicios.java
+++ Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/servicios/MovimientoDineroServicios.java
@@ -1,7 +1,11 @@
 package com.MasterTeam.Sprint2MasterTeam.servicios;
 
+import com.MasterTeam.Sprint2MasterTeam.entidades.Empresa;
+import com.MasterTeam.Sprint2MasterTeam.entidades.MovimientoDinero;
 import com.MasterTeam.Sprint2MasterTeam.repositorios.MovimientoDineroRepositorio;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovimientoDineroServicios {
@@ -10,6 +14,18 @@ public class MovimientoDineroServicios {
 
     public MovimientoDineroServicios(MovimientoDineroRepositorio repositorioMov){
         this.repositorioMov = repositorioMov;
+    }
+    //metodo para consultar todas los movimientos de dinero
+    public List<MovimientoDinero> getRepositorioMov(){
+        return this.repositorioMov.findAll();
+    }
+
+    //metodo para crear un movimiento de dinero
+    public MovimientoDinero crearMovimientoDinero(MovimientoDinero nuevaMovDinero){
+        return this.repositorioMov.save(nuevaMovDinero);
+    }
+    public MovimientoDinero eliminarMovimientoDinero(MovimientoDinero eliminarMovDinero) {
+        return this.repositorioMov.save(eliminarMovDinero);
     }
 
 }
