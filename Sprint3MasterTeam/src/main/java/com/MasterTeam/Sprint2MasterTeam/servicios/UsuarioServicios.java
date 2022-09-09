3e3e9bae4ced68337b8127e98a50b12a3243a40f
jgarciar820 <jgarciar820@gmail.com>
2022-09-09 00:36:08 -0500

    usuarios

--- Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/servicios/UsuarioServicios.java
+++ Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/servicios/UsuarioServicios.java
@@ -8,13 +8,20 @@ public class UsuarioServicios {
 
     private UsuarioRepositorio repositorioUsu;
 
-    public UsuarioServicios(UsuarioRepositorio repositorioUsu){
+    public UsuarioServicios(UsuarioRepositorio repositorioUsu) {
         this.repositorioUsu = repositorioUsu;
     }
 
     //metodo para consultar todos los usuarios
+    public List<Empleados> getlistaEmplea(){
+        return repositorioUsu.findAll();
+    }
     //metodo para consultar un usuario
+    
     //metodo para crear un usuario
+    public Empleado crearEmpleado(Empleado nuevoEmpleado){
+        return repositorioUsu.save(nuevoEmpleado);
+    }
     //metodo para editar un usuario
     //metodo para eliminar un usuario
 
