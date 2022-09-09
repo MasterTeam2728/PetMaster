1796d6f1965b363070627bf7467365520a4f7f60
jgarciar820 <jgarciar820@gmail.com>
2022-09-08 21:56:17 -0500

    Users

--- Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/controladores/UsuariosControlador.java
+++ Sprint3MasterTeam/src/main/java/com/MasterTeam/Sprint2MasterTeam/controladores/UsuariosControlador.java
@@ -1,6 +1,9 @@
 package com.MasterTeam.Sprint2MasterTeam.controladores;
 
 import com.MasterTeam.Sprint2MasterTeam.servicios.UsuarioServicios;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +18,17 @@ public class UsuariosControlador {
     //El sistema devuelve reponses 200 en la ruta /users con los siguientes verbos:
     //GET, POST
 
+    //METODO CONSULTA DE USUARIOS
+    @GetMapping("/users")
+    public List<Usuario> listaUsuario(){
+        return serviciosUsu.getlistaUsu();
+    }
+
+    // METODO CONSULTA DE UN USUARIO
+    @PostMapping("/users")
+    public Usuario crearUsuario(@RequestBody Usuario u){
+        return serviciosUsu.crearUsuario(u);
+    }
     //El sistema devuelve responses 200 en la ruta /user/[id] con los siguientes verbos:
     //GET, PATCH, DELETE
 
